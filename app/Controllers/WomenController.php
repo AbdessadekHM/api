1a52268b4144c0d695f1ac02326297a38b6dfa69
Abdessadek Hmiddouch <abdessadekhm@gmail.com>
2024-06-21 19:56:42 +0100

    Add scan feature

--- app/Controllers/WomenController.php
+++ app/Controllers/WomenController.php
@@ -1,8 +1,25 @@
 <?php
 
 class WomenController extends AuthController {
+    private $product;
     function __construct(){
         parent::__construct("womens");
+        $this->product = new Product();
     }
-
+    function scan(){
+        if($_SERVER["REQUEST_METHOD"]=="POST"){
+           $input = $this->api->recieveJsonInput();
+           extract($input); 
+           $result = $this->user->getFilteredUsers("username='$username'");
+           if(!$result){
+            $this->product->addItem($input);
+            $this->api->sendJsonResponse({"message" : "Product Added Successfully"},200)
+           }
+           else{
+            $this->api->sendJsonResponse({"message" : "Product Already exist"},200)
+           }
+            
+        }
+        
+    }
 }
