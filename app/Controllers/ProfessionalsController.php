6dd987fbbf24478e4034392490bed6b7485e7c0c
Abdessadek Hmiddouch <abdessadekhm@gmail.com>
2024-06-21 19:37:41 +0100

    Add Professoinals Controllers

---
+++ app/Controllers/ProfessionalsController.php
@@ -0,0 +1,8 @@
+<?php
+
+class ProfessionalController extends AuthController {
+    function __construct(){
+        parent::__construct("professionals");
+    }
+
+}
---
+++ app/Controllers/WomenController.php
@@ -0,0 +1,8 @@
+<?php
+
+class WomenController extends AuthController {
+    function __construct(){
+        parent::__construct("womens");
+    }
+
+}
